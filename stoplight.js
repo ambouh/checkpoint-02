27d7e9c40f3ecd3cb8b7023f36c4370eb3071c98
ambouh <aphricanboi@gmail.com>
2017-02-08 12:18:17 -0500

    From JS Part-II(2) to Done

--- stoplight.js
+++ stoplight.js
@@ -1,3 +1,18 @@
 $(document).ready(
-    $
+    $('.bulb').on('click', function () {
+            bulb = $(this);
+
+        if(bulb.hasClass('active')) {
+            bulb.removeClass('active');
+        } else{
+            bulb.addClass('active');
+            bulb.siblings().removeClass('active');
+
+            if (bulb.attr('id') === 'yellow') {
+                setTimeout(function () {
+                    $('#red').click();
+                }, 2000);
+            }
+        }
+    })
 );
